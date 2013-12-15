25e3399935c8082990b4b27058e410baa0936fa7
Ben Davis <bendavis78@gmail.com>
2013-12-15 13:02:41 -0600

    pep8 fixes

--- template_email/templatetags/email.py
+++ template_email/templatetags/email.py
@@ -1,11 +1,15 @@
 from django.template import Library, Node
+
+
 register = Library()
+
 
 def do_email_tag(parser, token):
     tag = token.contents
     nodelist = parser.parse(('end%s' % tag,))
     parser.delete_first_token()
     return EmailNode(tag, nodelist)
+
 
 class EmailNode(Node):
     def __init__(self, tag, nodelist):
@@ -18,6 +22,7 @@ class EmailNode(Node):
             return ''
         return self.nodelist.render(context)
 
-register.tag('subject',  do_email_tag)
-register.tag('body',     do_email_tag)
+
+register.tag('subject', do_email_tag)
+register.tag('body', do_email_tag)
 register.tag('bodyhtml', do_email_tag)
